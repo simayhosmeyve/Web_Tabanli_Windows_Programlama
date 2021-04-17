d9b968f9ed20939ddb6fb74a4da0e95c5162f1ed
simayhosmeyve <44275183+simayhosmeyve@users.noreply.github.com>
2021-04-17 04:15:43 +0300

    Add files via upload

---
+++ HesapMakinesi/Calculator.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="HesapMakinesi.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Hesap Makinesi</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div ><asp:TextBox ID="TextBox" runat="server" Width="192px" Height="40px"  Font-Bold="true" Font-Size="20px" AutoPostBack="True"></asp:TextBox>
+            <br/>
+            <asp:Button ID="Button1" Height="60px" Width="50px" Font-Size="Large" Text-size="100px" runat="server" Text="7" OnClick="Button_Click" /><asp:Button ID="Button2"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="8" OnClick="Button_Click" /><asp:Button ID="Button3"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="9" OnClick="Button_Click" /><asp:Button ID="Button4"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="/" OnClick="Button_Click" />
+            <br/>
+            <asp:Button ID="Button5"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="4" OnClick="Button_Click" /><asp:Button ID="Button6"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="5" OnClick="Button_Click" /><asp:Button ID="Button7"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="6" OnClick="Button_Click" /><asp:Button ID="Button8"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="*" OnClick="Button_Click" />
+            <br/>
+            <asp:Button ID="Button9"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="1" OnClick="Button_Click" /><asp:Button ID="Button10"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="2" OnClick="Button_Click" /><asp:Button ID="Button11"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="3" OnClick="Button_Click" /><asp:Button ID="Button12"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="-" OnClick="Button_Click" />
+            <br/>
+            <asp:Button ID="Button13"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="0" OnClick="Button_Click" /><asp:Button ID="Button14"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="+" OnClick="Button_Click" /><asp:Button ID="Button15" Height="60px" Width="50px" Font-Size="Large" runat="server" Text="CLC" OnClick="Button_Click" /><asp:Button ID="Button16"  Height="60px" Width="50px" Font-Size="Large" runat="server" Text="=" OnClick="Button_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ HesapMakinesi/Calculator.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HesapMakinesi
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        static double num1, num2, outcome;
+        static String calc;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBox.Text = "0";
+            }
+        }
+
+       
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            switch (B.ClientID)
+            {    
+                case "Button1":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "7";
+                    else TextBox.Text += "7";
+                    break;
+
+                case "Button2":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "8";
+                    else TextBox.Text += "8";
+                    break;
+
+                case "Button3":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "9";
+                    else TextBox.Text += "9";
+                    break;
+
+                case "Button4":
+                    calc = "/";
+                    num1 = Convert.ToDouble(TextBox.Text);
+                    TextBox.Text = "0";
+                    break;
+
+                case "Button5":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "4";
+                    else TextBox.Text += "4";
+                    break;
+
+                case "Button6":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "5";
+                    else TextBox.Text += "5";
+                    break;
+
+                case "Button7":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "6";
+                    else TextBox.Text += "6";
+                    break;
+
+                case "Button8":
+                    calc = "*";
+                    num1 = Convert.ToDouble(TextBox.Text);
+                    TextBox.Text = "0";
+                    break;
+
+                case "Button9":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "1";
+                    else TextBox.Text += "1";
+                    break;
+
+                case "Button10":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "2";
+                    else TextBox.Text += "2";
+                    break;
+
+                case "Button11":
+                    if (TextBox.Text == "0")
+                        TextBox.Text = "3";
+                    else TextBox.Text += "3";
+                    break;
+
+                case "Button12":
+                    calc = "-";
+                    num1 = Convert.ToDouble(TextBox.Text);
+                    TextBox.Text = "0";
+                    break;
+
+                case "Button13":
+                    if (TextBox.Text != "0")
+                        TextBox.Text += "0";
+                    break;
+
+                case "Button14":
+                    calc = "+";
+                    num1 = Convert.ToDouble(TextBox.Text);
+                    TextBox.Text = "0";
+                    break;
+
+                case "Button15":
+                    //CLC
+                    TextBox.Text = "0";
+                    break;
+
+                case "Button16":
+                    //=
+                    num2 = Convert.ToDouble(TextBox.Text);
+                    if (calc == "+")
+                    {
+                        double outcome = num1 + num2;
+                        TextBox.Text = Convert.ToString(outcome);
+                    }
+
+                    if (calc == "/")
+                    {
+                        double outcome = num1 / num2;
+                        TextBox.Text = Convert.ToString(outcome);
+                    }
+
+                    if (calc == "*")
+                    {
+                        double outcome = num1 * num2;
+                        TextBox.Text = Convert.ToString(outcome);
+                    }
+
+                    if (calc == "-")
+                    {
+                        double outcome = num1 - num2;
+                        TextBox.Text = Convert.ToString(outcome);
+                    }
+                    break;
+            }
+            }
+        }
+        
+}
